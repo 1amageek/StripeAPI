51cfd32c79a6464625dd420bea78e9f29fc3d933
1amageek <tmy0x3@icloud.com>
2017-10-01 19:03:52 +0900

    first commit

---
+++ StripeAPI/StripeAPI.h
@@ -0,0 +1,19 @@
+//
+//  StripeAPI.h
+//  StripeAPI
+//
+//  Created by 1amageek on 2017/10/01.
+//  Copyright © 2017年 Stamp Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StripeAPI.
+FOUNDATION_EXPORT double StripeAPIVersionNumber;
+
+//! Project version string for StripeAPI.
+FOUNDATION_EXPORT const unsigned char StripeAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StripeAPI/PublicHeader.h>
+
+
